58096569402ee099abba3a0b46efb47560868979
liqiang <ken.li@inspiry.cn>
2019-12-24 14:51:44 +0800

    更新快捷方式

---
+++ CodeStyle/vshotkey.h
@@ -0,0 +1,61 @@
+/*
+vscdoe常用快捷键(使用默认的键盘布局)
+F1 或 Ctrl+Shift+P: 打开命令面板。
+按一下 Backspace 会进入到 Ctrl+P 模式
+在 Ctrl+P 下输入 > 可以进入 Ctrl+Shift+P 模式
+在 Ctrl+P 窗口下还可以:
+直接输入文件名，跳转到文件
+? 列出当前可执行的动作
+! 显示 Errors或 Warnings，也可以 Ctrl+Shift+M
+: 跳转到行数，也可以 Ctrl+G 直接进入
+@ 跳转到 symbol（搜索变量或者函数），也可以 Ctrl+Shift+O 直接进入
+@ 根据分类跳转 symbol，查找属性或函数，也可以 Ctrl+Shift+O 后输入:进入
+# 根据名字查找 symbol，也可以 Ctrl+T
+常用快捷键
+打开一个新窗口： Ctrl+Shift+N
+关闭窗口： Ctrl+Shift+W
+新建文件 Ctrl+N
+文件之间切换 Ctrl+Tab
+切出一个新的编辑器（最多 3 个） Ctrl+\，也可以按住 Ctrl 鼠标点击 Explorer 里的文件名
+左中右 3 个编辑器的快捷键 Ctrl+1 Ctrl+2 Ctrl+3
+3 个编辑器之间循环切换 Ctrl+
+编辑器换位置， Ctrl+k然后按 Left或 Right
+代码行缩进 Ctrl+[ 、 Ctrl+]
+Ctrl+C 、 Ctrl+V 复制或剪切当前行/当前选中内容
+代码格式化： Shift+Alt+F，或 Ctrl+Shift+P 后输入 format code
+上下移动一行： Alt+Up 或 Alt+Down
+向上向下复制一行： Shift+Alt+Up 或 Shift+Alt+Down
+在当前行下边插入一行 Ctrl+Enter
+在当前行上方插入一行 Ctrl+Shift+Enter
+移动到行首： Home
+移动到行尾： End
+移动到文件结尾： Ctrl+End
+移动到文件开头： Ctrl+Home
+移动到定义处： F12
+定义处缩略图：只看一眼而不跳转过去 Alt+F12
+移动到后半个括号： Ctrl+Shift+]
+选择从光标到行尾： Shift+End
+选择从行首到光标处： Shift+Home
+删除光标右侧的所有字： Ctrl+Delete
+扩展/缩小选取范围： Shift+Alt+Left 和 Shift+Alt+Right
+多行编辑(列编辑)：Alt+Shift+鼠标左键，Ctrl+Alt+Down/Up
+同时选中所有匹配： Ctrl+Shift+L
+Ctrl+D 下一个匹配的也被选中
+回退上一个光标操作:Ctrl+U
+找到所有的引用： Shift+F12
+同时修改本文件中所有匹配的： Ctrl+F2
+查找 Ctrl+F
+查找替换 Ctrl+H
+整个文件夹中查找 Ctrl+Shift+F
+全屏：F11
+zoomIn/zoomOut：Ctrl +/-/0
+侧边栏显/隐：Ctrl+B
+显示资源管理器 Ctrl+Shift+E
+显示搜索 Ctrl+Shift+F
+显示 Git Ctrl+Shift+G
+显示 Debug Ctrl+Shift+D
+显示 Output Ctrl+Shift+U
+
+
+
+*/
